9b8370446adee41fed0087f80ffcc5a8e76dd1bc
Washington Junior <w.r_1991@hotmail.com>
2023-05-22 22:25:36 -0300

    Cadastro dos produtos nessa aula

--- Site/urls.py
+++ Site/urls.py
@@ -3,6 +3,9 @@ from Site import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sobre-a-empresa', views.institucional, name='institucional'),
+    path('contato-shopping', views.contato, name='contato' ),
+    path('cadastro-shopping', views.cadastro, name='cadastro'),
     path('produtos', views.produto_lista, name='produto_lista'),
     path('produto', views.produto_detalhe, name='produto_detalhes')
 ]
